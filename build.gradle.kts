f4fe35414c53336386f134961cb2bba4a800df66
Nico D'Cotta <nico.dc@outlook.com>
2019-12-20 14:18:46 +0000

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+  kotlin("jvm") version "1.3.61"
+}
+
+group = "org.cottand"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  jcenter()
+  mavenCentral()
+  maven("https://dl.bintray.com/kotlin/kotlin-numpy")
+
+}
+
+val arrowVersion = "0.10.4"
+dependencies {
+  implementation(kotlin("stdlib-jdk8"))
+  implementation("org.jetbrains:kotlin-numpy:0.1.0")
+  implementation("io.arrow-kt:arrow-core:$arrowVersion")
+  implementation("io.arrow-kt:arrow-syntax:$arrowVersion")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+}
